3caf5a0f79ca4ef0d48d4d356fe596c48e53055e
Daniel Boadzie <boadziedaniel43@gmail.com>
2022-12-12 14:42:19 +0000

    Add responsive layout for dashboard
    Add header and footer sections

--- src/cards.py
+++ src/cards.py
@@ -4,13 +4,26 @@ from h2o_wave import Q, ui
 async def app_layout(q: Q):
     q.page["meta"] = ui.meta_card(
         box="",
+        themes=[
+            ui.theme(
+                name="my-awesome-theme",
+                primary="#13ebe7",
+                text="#ffffff",
+                card="#12123b",
+                page="#070b1a",
+            )
+        ],
+        # theme="my-awesome-theme",
         layouts=[
             ui.layout(
                 breakpoint="xs",
                 zones=[
-                    ui.zone("header"),
+                    ui.zone("header", align="start", justify="around"),
                     ui.zone(
                         name="zone1", direction=ui.ZoneDirection.COLUMN, size="100%", align="center", justify="center"
+                    ),
+                    ui.zone(
+                        name="zone2", direction=ui.ZoneDirection.COLUMN, size="100%", align="center", justify="center"
                     ),
                     ui.zone("footer"),
                 ],
@@ -19,8 +32,22 @@ async def app_layout(q: Q):
                 breakpoint="m",
                 zones=[
                     ui.zone("header"),
-                    ui.zone(name="zone1", direction=ui.ZoneDirection.ROW, size="50%"),
-                    ui.zone(name="zone2", direction=ui.ZoneDirection.ROW, size="50%"),
+                    ui.zone(
+                        "body",
+                        direction=ui.ZoneDirection.COLUMN,
+                        align="stretch",
+                        justify="between",
+                        size="60rem",
+                        zones=[
+                            ui.zone(
+                                "zone1", direction=ui.ZoneDirection.ROW, align="center", justify="around", size="50%"
+                            ),
+                            ui.zone("separate", direction="column", size="10px"),
+                            ui.zone(
+                                "zone2", direction=ui.ZoneDirection.ROW, align="center", justify="around", size="50%"
+                            ),
+                        ],
+                    ),
                     ui.zone("footer"),
                 ],
             ),
@@ -30,7 +57,7 @@ async def app_layout(q: Q):
 
 async def home(q: Q):
     q.page["header"] = ui.header_card(
-        box=ui.box("header", width="100%", height="86px"),
+        box=ui.box("header", width="100%", height="80px"),
         icon="home",
         icon_color="white",
         title="Music and its effect on Mental Health",
